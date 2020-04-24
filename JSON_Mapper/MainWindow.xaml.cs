024803e2f099b5829aeb8ec00aaa1506edd0591d
RT3vec <robert_terravecchia@student.uml.edu>
2020-04-24 15:47:58 -0400

    Added JSON object class

--- JSON_Mapper/MainWindow.xaml.cs
+++ JSON_Mapper/MainWindow.xaml.cs
@@ -85,32 +85,68 @@ namespace JSON_Mapper
             var gridView = new GridView();
             this.listView.View = gridView;
             int i = 0;
-            string text = System.IO.File.ReadAllText("TestJson.txt");
+            string text = System.IO.File.ReadAllText("test.json");
             JsonTextReader reader = new JsonTextReader(new StringReader(text));
             JsonColumn test = new JsonColumn { Property = "", Value = "" };
 
-            while (reader.TokenType.ToString() != "String")
+            JsonTreeWrapper masterTree = new JsonTreeWrapper();
+            String jProp = "", jVal = "";
+            List<String> jValList = new List<String>();
+            bool arrayFlag = false;
+
+            //Parse JSON
+            while (reader.Read())
             {
-                reader.Read();
-                
+                //If a property type save value 
                 if (reader.TokenType.ToString() == "PropertyName")
                 {
-                    test.Property = reader.Value.ToString();
-                } else if (reader.TokenType.ToString() == "String")
+                    //test.Property = reader.Value.ToString();
+                    jProp = reader.Value.ToString();
+                }
+                //if a string value, set it and add to master tree
+                else if (reader.TokenType.ToString() == "String" || reader.TokenType.ToString() == "Integer")
                 {
-                    test.Value = reader.Value.ToString();
+                    if (!arrayFlag)
+                    {
+                        //test.Value = reader.Value.ToString();
+                        jVal = reader.Value.ToString();
+                        masterTree.addObject(new JsonTreeObject(jProp, jVal));
+                        jProp = "";
+                        jVal = "";
+                    }
+                    else
+                    {
+                        jValList.Add(reader.Value.ToString());
+                    }
+                }
+                else if (reader.TokenType.ToString() == "StartArray")
+                {
+                    arrayFlag = true;
+                }
+                else if (reader.TokenType.ToString() == "EndArray")
+                {
+                    masterTree.addObject(new JsonTreeObject(jProp, jValList));
+                    arrayFlag = false;
+                    jValList.Clear();
                 }
 
 
             }
 
+            masterTree.printTree();
+
+            
+            
+
+            
             gridView.Columns.Add(new GridViewColumn
             {
                 Header = "Property",
                 DisplayMemberBinding = new Binding("Property")
             });
 
-            this.listView.Items.Add(test);
+            this.listView.Items.Add(new JsonColumn {Property = masterTree.data[0].property, Value = masterTree.data[0].values[0] } );
+            
 
         }
 
@@ -151,6 +187,69 @@ namespace JSON_Mapper
              this.listView.Items.Add(new MyItem { Id = 1, Name = "David" });
          }*/
 
+
+        public class JsonTreeWrapper
+        {
+            //root list of tree
+            public List<JsonTreeObject> data = new List<JsonTreeObject>();
+
+
+            public void addObject(JsonTreeObject newObject)
+            {
+                data.Add(newObject);
+            }
+
+
+            public void printTree()
+            {
+                foreach(JsonTreeObject obj in data)
+                {
+                    obj.printTreeObject();
+                }
+            }
+            
+
+
+        }
+
+        public class JsonTreeObject
+        {
+            public String property;
+            public List<String> values = new List<string>();
+            public bool isArray;
+
+            public JsonTreeObject(String p, String v)
+            {
+                property = p;
+                values.Add(v);
+                isArray = false;
+            }
+
+            public JsonTreeObject(String p, List<String> v)
+            {
+                property = p;
+                values = new List<String>(v);
+                isArray = true;
+            }
+
+            public void printTreeObject()
+            {
+                if(!isArray)
+                {
+                    Console.WriteLine("Token {0}, Value {1}", property, values[0]);
+                }
+                else
+                {
+                    Console.WriteLine("Token {0}", property);
+                    foreach (String obj in values)
+                    {
+                        Console.WriteLine("Array Value: {0}", obj);
+                    }
+                }
+                
+            }
+
+        }
 
         public class MyItem
         {
